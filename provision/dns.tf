bfc5e7c4fee8c141be19b02505842a9ea255b268
Reeto Chatterjee <reetobratachatterjee@gmail.com>
2021-02-05 02:45:14 +0000

    Add a new subdomain for mumble server

--- provision/dns.tf
+++ provision/dns.tf
@@ -23,19 +23,38 @@ resource "cloudflare_record" "aaaa" {
   value   = hcloud_server.main.ipv6_address
 }
 
+locals {
+  domain_aliases = ["mumble"]
+}
+
+resource "cloudflare_record" "aliases" {
+  zone_id = local.cloudflare_zone_id
+  name = each.key
+  type = "CNAME"
+  value = local.fqdn
+
+  for_each = toset(local.domain_aliases)
+}
+
 # application-specific
+locals {
+  mumble_srv_names = [cloudflare_record.aliases["mumble"].name, var.hostname]
+}
+
 resource "cloudflare_record" "mumble_srv" {
   zone_id = local.cloudflare_zone_id
-  name    = "_mumble._tcp.${var.hostname}"
+  name    = "_mumble._tcp.${each.key}"
   type    = "SRV"
 
   data = {
     service  = "_mumble"
     proto    = "_tcp"
-    name     = var.hostname
+    name     = each.key
     priority = 0
     weight   = 1
     port     = 1337
     target   = local.fqdn
   }
+
+  for_each = toset(local.mumble_srv_names)
 }
